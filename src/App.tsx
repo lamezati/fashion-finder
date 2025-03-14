11174526de28c7bbfe718e3d992af33eff36edbf
Leonel Mezatio <afrokab1@gmail.com>
2025-03-13 20:31:44 -0400

    Restore logo in mobile header

--- src/App.tsx
+++ src/App.tsx
@@ -135,9 +135,13 @@ function App() {
               <div className="max-w-7xl mx-auto px-4">
                 <div className="flex justify-between h-14 items-center">
                   <div className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                      <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-                    </svg>
+                    {/* Logo */}
+                    <img 
+                      src="/fashion-icon.svg" 
+                      alt="FashionFinder Logo" 
+                      className="h-8 w-8 text-white"
+                      style={{ filter: 'brightness(0) invert(1)' }} // Make SVG white
+                    />
                     <span className="ml-2 text-xl font-semibold text-white">FashionFinder</span>
                   </div>
                   {user && (
